a6df9fdaa3652fa9ea65ce40e941fa17a13a7386
Hiroki KIYOHARA <hirokiky@gmail.com>
2016-01-18 16:23:42 +0900

    Fixed bug on packaging

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 setup(
     name='cookiepatcher',
     description='A tool to apply updates of cookiecutter templates',
-    packages=find_packages('cookiepatcher'),
+    packages=find_packages(),
     install_requires=['cookiecutter'],
     entry_points={
         'console_scripts': [
