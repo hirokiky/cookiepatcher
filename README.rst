fd30caf47c1032a61eacc744694a515202cddeb2
Hiroki KIYOHARA <hirokiky@gmail.com>
2016-01-18 16:17:22 +0900

    Support config

--- README.rst
+++ README.rst
@@ -10,5 +10,24 @@ Usage
 
 To apply changes of `audreyr/cookiecutter-pypackage`, type like this at your project repository::
 
-    cookiepatcher gh:audreyr/cookiecutter-pypackage HEAD~
+    cookiepatcher
 
+And specify cookiecutter template and revisions to apply::
+
+    Input template repository url: gh:audreyr/cookiecutter-pypackage
+    Input template version applied currently: 5a519e6f19655468332ff95990bb7d107571061c
+    Input version to follow [master]: master
+
+
+Or you can specify from arguments::
+
+    cookiepatcher --template gh:audreyr/cookiecutter-pypackage --diff 5a519e6f19655468332ff95990bb7d107571061c master
+
+
+From the second time
+--------------------
+
+Cookiepatcher will create `cookiepatcher.json` config file.
+If you have this file on the repository root,  cookiepatcher won't ask you anything
+(template url, revisions, variables).
+It will apply patches to follow `master` of the cookiecutter template.
--- cookiepatcher/main.py
+++ cookiepatcher/main.py
@@ -1,4 +1,5 @@
 import argparse
+import json
 import os
 import subprocess
 
@@ -11,28 +12,52 @@ from cookiecutter.vcs import clone
 from jinja2 import Template
 
 
+CONF_PATH = './cookiepatcher.json'
+
+
 def cookiepatch():
     parser = argparse.ArgumentParser(description='Tool to apply / create patch from '
                                                  'cookiecutter templates')
-    parser.add_argument('template', type=str,
+    parser.add_argument('--template', type=str,
                         help='an integer for the accumulator')
-    parser.add_argument('diff', type=str, nargs='+',
+    parser.add_argument('--diff', type=str, nargs='+',
                         help='versions passed for git diff')
     parser.add_argument('--show', action='store_true',
                         help='Just print diff')
 
     args = parser.parse_args()
 
+    conf_file = None
+    if os.path.exists(CONF_PATH):
+        with open(CONF_PATH) as f:
+            conf_file = json.load(f)
+
+    if args.template:
+        template = args.template
+    elif conf_file and 'template' in conf_file:
+        template = conf_file['template']
+    else:
+        template = input('Input template repository url: ')
+
+    if args.diff:
+        diff = args.diff
+    elif conf_file and 'revision' in conf_file:
+        diff = [conf_file['revision']]
+    else:
+        cur = input('Input template version applied currently: ')
+        to = input('Input version to follow [master]: ') or 'master'
+        diff = [cur, to]
+
     no_input = False
 
     config_dict = get_user_config(config_file=USER_CONFIG_PATH)
-    template = expand_abbreviations(args.template, config_dict)
+    parsed_template = expand_abbreviations(template, config_dict)
 
-    repo_dir = clone(repo_url=template,
+    repo_dir = clone(repo_url=parsed_template,
                      clone_to_dir=config_dict['cookiecutters_dir'],
                      checkout=None, no_input=no_input)
     patch_bytes = subprocess.check_output(['git', 'diff'] +
-                                          args.diff +
+                                          diff +
                                           ['--', '{{cookiecutter.repo_name}}'],
                                           cwd=repo_dir)
     patch_str = patch_bytes.decode()
@@ -43,9 +68,12 @@ def cookiepatch():
         default_context=config_dict['default_context'],
         extra_context={},
     )
-    # prompt the user to manually configure at the command line.
-    # except when 'no-input' flag is set
-    context['cookiecutter'] = prompt_for_config(context, no_input)
+    if conf_file:
+        context['cookiecutter'] = conf_file['variables']
+    else:
+        # prompt the user to manually configure at the command line.
+        # except when 'no-input' flag is set
+        context['cookiecutter'] = prompt_for_config(context, no_input)
 
     rendered = Template(patch_str).render(**context)
 
@@ -53,8 +81,25 @@ def cookiepatch():
         print(rendered)
         return
 
-    p = subprocess.Popen(['patch',  '-Np1', '--no-backup-if-mismatch'], stdin=subprocess.PIPE, cwd='..')
+    p = subprocess.Popen(['patch',  '-Np1', '--no-backup-if-mismatch'], stdin=subprocess.PIPE,
+                         cwd='..')
     p.communicate(rendered.encode())
+
+    # Generate cookiepatcher JSON
+    if len(diff) == 1:
+        rev = 'HEAD'
+    else:
+        rev = diff[-1]
+    revision_bytes = subprocess.check_output(['git', 'rev-parse'] + [rev],
+                                             cwd=repo_dir)
+    revision_str = revision_bytes.decode().rstrip('\n')
+    json_content = {
+        'revision': revision_str,
+        'variables': context['cookiecutter'],
+        'template': template
+    }
+    with open(CONF_PATH, 'w') as f:
+        json.dump(json_content, f)
 
 
 if __name__ == '__main__':
