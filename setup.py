bd317ea00de1c2a9cb6e35e1589a0a00c6c79644
Hiroki KIYOHARA <hirokiky@gmail.com>
2016-01-18 16:26:04 +0900

    Added version info

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='cookiepatcher',
+    version='0.1',
     description='A tool to apply updates of cookiecutter templates',
     packages=find_packages(),
     install_requires=['cookiecutter'],
